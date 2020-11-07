1ca583661809c661614ef5f1856557706730aeaf
Nestop <fednestor99@gmail.com>
2020-11-07 18:29:39 +0300

    Simplifying the RacketController code

--- Assets/Scripts/Game/RacketController.cs
+++ Assets/Scripts/Game/RacketController.cs
@@ -6,8 +6,7 @@ namespace Game
     {
         [SerializeField] private RectTransform rectTransform;
         [SerializeField] private Rigidbody2D rigidBody2D;
-    
-        private float _halfScreenWidth;
+        
         private float _leftBorder;
         private float _rightBorder;
         private float _lastX;
@@ -15,16 +14,14 @@ namespace Game
         private void Awake()
         {
             var halfScaleX = transform.localScale.x / 2f;
-            _halfScreenWidth = Screen.width / 2f;
-            _leftBorder = /*-_halfScreenWidth*/ + halfScaleX;
-            _rightBorder = 2*_halfScreenWidth - halfScaleX;
-            _lastX = Input.mousePosition.x;// -_halfScreenWidth + 
+            _leftBorder = halfScaleX;
+            _rightBorder = Screen.width - halfScaleX;
+            _lastX = Input.mousePosition.x;
         }
-
-
+        
         private void FixedUpdate()
         {
-            var x = Input.mousePosition.x;//-_halfScreenWidth + 
+            var x = Input.mousePosition.x;
             x = Mathf.Clamp(x, _leftBorder, _rightBorder);
 
             var delta = x - _lastX;
@@ -36,10 +33,7 @@ namespace Game
                 Quaternion.Slerp(locRot, Quaternion.identity, 0.15f) : 
                 Quaternion.Slerp(locRot, Quaternion.Euler(Vector3.forward * -delta) * locRot, 0.15f);
             var position = Vector2.Lerp(worldPos, new Vector2(x, worldPos.y), 0.1f);
-            
-            //rectTransform.localRotation = rotation;
-            //rectTransform.anchoredPosition = position;
-            
+
             rigidBody2D.MovePosition(position);
             rigidBody2D.MoveRotation(rotation);
 
